d39dfa68acc8e1203355e5d05d339795b803298c
pxp928 <parth.psu@gmail.com>
2022-06-26 16:19:54 -0400

    changed main function
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+type tyHandler struct{}
+
+func (h tyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("at=request path=%s", r.URL.Path)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "ty!")
+}
+
 func main() {
-	fmt.Println("hello")
+	err := http.ListenAndServe(":8080", tyHandler{})
+	log.Fatal(err)
 }
